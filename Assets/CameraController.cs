909a212924469436062fb6d1ebde617776b8c4d3
Ricardo Maginador <ricardo.maginador@wildlifestudios.com>
2021-12-17 15:28:18 -0300

    add camera move with drag feature

---
+++ Assets/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private Camera viewCamera;
+    [SerializeField] private float clickTimeThreshold,clickTimer;
+    private Vector3 recordedInitialPosition, recordedFinalPosition;
+
+    private bool isClicking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            recordedInitialPosition = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+            clickTimer = Time.time;
+            isClicking = true;
+        }
+
+        if (isClicking)
+        {
+            if (Time.time - clickTimer > clickTimeThreshold)
+            {
+                recordedFinalPosition = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+                viewCamera.transform.position += recordedInitialPosition - recordedFinalPosition;
+            }
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            
+            isClicking = false;
+
+        }
+    }
+}
